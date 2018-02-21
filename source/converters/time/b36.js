98f589a56663ccd30b8422d6321567c1018158b3
Aran <aran@ermarian.net>
2018-02-21 14:37:45 +0100

    Add base36 time page.

---
+++ source/converters/time/b36.js
@@ -0,0 +1,60 @@
+const ui = ($ => {
+  const dom = {
+    decimal: $('#decimal'),
+    b36: $('#b36'),
+    play: $('#play'),
+    pause: $('#pause'),
+    digits: Array.from($('article table').querySelectorAll('tr')).slice(1)
+  };
+
+  const todate = number => new Date(1000 * number);
+  const toDig = number => number.toString().padStart(2, 0);
+  const toDateString = date => `${date.getFullYear()}-${toDig(date.getMonth())}-${toDig(date.getDate())}`
+                      + ` ${toDig(date.getHours())}:${toDig(date.getMinutes())}:${toDig(date.getSeconds())}`;
+  const fromdate = date => Math.floor(date.getTime() / 1000);
+  const tob36 = number => number.toString(36).padStart(6, 0);
+  const fromb36 = number => parseInt(number.substring(0, 6), 36);
+  const state = {
+    playing: null
+  };
+
+  const refresh = time => {
+    time = time || (new Date());
+    dom.decimal.valueAsNumber = time.getTime();
+    const b36 = dom.b36.value = tob36(fromdate(time));
+    for (let i = 0; i < 6; i++) {
+      const digit = b36[i];
+      const prefix = b36.substring(0, i+1);
+      const start = todate(fromb36(prefix + '0'.repeat(5-i)));
+      const end = todate(fromb36(prefix + 'z'.repeat(5-i)) + 1);
+      const cells = dom.digits[i].querySelectorAll('td');
+      cells[0].innerText = digit;
+      cells[1].innerText = toDateString(start);
+      cells[2].innerText = toDateString(end);
+    }
+  };
+
+  const buttonToggle = state => {
+    dom.play.style.display = state ? 'none' : '';
+    dom.pause.style.display = state ? '' : 'none';
+  }
+
+  const play = () => buttonToggle(state.playing = state.playing || setInterval(refresh, 1000)) || refresh();
+  const stop = () => buttonToggle(state.playing = state.playing && clearInterval(state.playing));
+
+  dom.play.onclick = play;
+  dom.decimal.onfocus = dom.b36.onfocus = dom.pause.onclick = stop;
+  dom.decimal.onchange = dom.b36.onchange = function () {
+    const time = this.id === 'decimal' ? new Date(this.valueAsNumber) : todate(fromb36(this.value));
+    refresh(time);
+  }
+
+  play();
+
+  return {
+    dom,
+    refresh,
+    play,
+    stop
+  };
+})(document.querySelector.bind(document));

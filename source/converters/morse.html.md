cf0c7748b8b571b85e8aca2239200406988ea1c8
Aran <aran@ermarian.net>
2018-02-22 13:35:19 +0100

    Add Morse converter.

---
+++ source/converters/morse.html.md
@@ -0,0 +1,11 @@
+---
+title: Morse code
+scripts: ['morse.js']
+---
+Enter text, or Morse code in the format `-- --- .-. ... . / -.-. --- -.. .`.
+
+<textarea id="input"></textarea>
+
+<span id="mode"></span>
+
+<div id="output" class="box code"></div>
---
+++ source/converters/morse.js
@@ -0,0 +1,84 @@
+const morse = {
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "A": ".-",
+  "B": "-...",
+  "C": "-.-.",
+  "D": "-..",
+  "E": ".",
+  "F": "..-.",
+  "G": "--.",
+  "H": "....",
+  "I": "..",
+  "J": ".---",
+  "K": "-.-",
+  "L": ".-..",
+  "M": "--",
+  "N": "-.",
+  "O": "---",
+  "P": ".--.",
+  "Q": "--.-",
+  "R": ".-.",
+  "S": "...",
+  "T": "-",
+  "U": "..-",
+  "V": "...-",
+  "W": ".--",
+  "X": "-..-",
+  "Y": "-.--",
+  "Z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "-": "-....-",
+  "'": ".----.",
+  ":": "---...",
+  "\"": ".-..-.",
+  "/": "-..-.",
+  "@": ".--.-.",
+  " ": "/"
+};
+
+const ui = ($ => {
+  const dom = {
+    input: $('#input'),
+    output: $('#output'),
+    mode: $('#mode')
+  };
+
+  const reverse_morse = {};
+  for (let key in morse) {
+    reverse_morse[morse[key]] = key;
+  }
+
+  const toMorse = text => Array
+    .from(text.replace(/\s+/g, ' ').toUpperCase())
+    .map(c => morse[c] || '')
+    .filter(c => c)
+    .join(' ');
+
+  const fromMorse = morse => morse
+    .split(/\s+/)
+    .map(c => reverse_morse[c] || `[bad code ${c} ]`)
+    .join('');
+
+  const isMorse = text => text.match(/^[\s.\/-]*$/);
+
+  dom.input.onkeyup = () => {
+    const input = dom.input.value;
+    const m = isMorse(input);
+    dom.mode.innerText = input ? (m ? 'Decoding Morse:' : 'Encoding to Morse:') : '';
+    dom.output.innerText = input ? (m ? fromMorse : toMorse)(input) : '';
+  }
+
+  return {dom, toMorse, fromMorse, isMorse};
+
+})(document.querySelector.bind(document));

a72add37ba8da549370cd51b4b9bbe30138e9355
Aran <aran@ermarian.net>
2018-02-22 03:02:27 +0100

    Put mtime in footer.
    Not fully reliable, but stable enough on a persistent build environment.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,6 @@
-// This is where it all goes :)
+($$ => {
+  for (let x of $$('span.time[data-time]')) {
+    const time = moment(+x.getAttribute('data-time')*1000);
+    x.innerText = time.fromNow();
+  }
+})(document.querySelectorAll.bind(document));

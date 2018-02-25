3257c18c444532026c522115c6ac8b827bd49757
Aran <aran@ermarian.net>
2018-02-25 02:12:06 +0100

    Set tooltip to formatted time.
    (This is done locally to get user timezone.)

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,7 @@
 ($$ => {
   for (let x of $$('span.time[data-time]')) {
     const time = moment(+x.getAttribute('data-time')*1000);
+    x.title = time.format('YYYY-MM-DD HH:mm:ss ZZ');
     x.innerText = time.fromNow();
   }
 })(document.querySelectorAll.bind(document));

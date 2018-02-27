3636fb02099866e2c7b3b03667be18da453ccc20
Aran <aran@ermarian.net>
2018-02-27 04:24:48 +0100

    Some broken editor added those frigging tab characters again.

--- source/utilities/password.js
+++ source/utilities/password.js
@@ -1,34 +1,34 @@
 const ui = (($, words) => {
-	const dom = {
-		output: $('#output'),
-		length: $('#length'),
-		count: $('#count'),
-		stats: $('#stats'),
-		cap: $('#cap'),
-		delimiter: $('#delimiter')
-	};
+  const dom = {
+    output: $('#output'),
+    length: $('#length'),
+    count: $('#count'),
+    stats: $('#stats'),
+    cap: $('#cap'),
+    delimiter: $('#delimiter')
+  };
 
-	const arrayRand = arr => arr[Math.floor(Math.random()*arr.length)];
-	const arrayRands = (arr, k) => Array(k).fill(0).map(_ => arrayRand(arr));
+  const arrayRand = arr => arr[Math.floor(Math.random()*arr.length)];
+  const arrayRands = (arr, k) => Array(k).fill(0).map(_ => arrayRand(arr));
 
-	const password = k => arrayRands(words, k)
-		.map(dom.cap.checked ? word => word.charAt().toUpperCase() + word.slice(1) : word => word)
-		.join(dom.delimiter.value);
-	const passwords = (k, n) => Array(n).fill(0).map(_ => password(k));
+  const password = k => arrayRands(words, k)
+    .map(dom.cap.checked ? word => word.charAt().toUpperCase() + word.slice(1) : word => word)
+    .join(dom.delimiter.value);
+  const passwords = (k, n) => Array(n).fill(0).map(_ => password(k));
 
-	const update = () => {
-		const length = +dom.length.value;
-		dom.output.innerText = passwords(length, +dom.count.value).join('\n');
-		const bits = Math.log2(words.length).toFixed(2);
-		dom.stats.innerText = `The word list contains ${words.length} words, so a password of length ${length} has ${bits*length} bits of entropy.`;
-	}
+  const update = () => {
+    const length = +dom.length.value;
+    dom.output.innerText = passwords(length, +dom.count.value).join('\n');
+    const bits = Math.log2(words.length).toFixed(2);
+    dom.stats.innerText = `The word list contains ${words.length} words, so a password of length ${length} has ${bits*length} bits of entropy.`;
+  }
 
-	dom.length.oninput = dom.count.oninput = dom.delimiter.oninput = dom.cap.onchange = update;
+  dom.length.oninput = dom.count.oninput = dom.delimiter.oninput = dom.cap.onchange = update;
 
-	update();
+  update();
 
-	return {update, dom, password, passwords}
+  return {update, dom, password, passwords}
 })(
-	document.querySelector.bind(document),
-	words
+  document.querySelector.bind(document),
+  words
 );

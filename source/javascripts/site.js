ca62ce8f63b1433c8c09ece9025af326041bb0ad
Aran <aran@ermarian.net>
2018-03-13 23:30:28 +0100

    Add an onclick toggle for dates.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,5 +3,8 @@
     const time = moment(x.dateTime);
     x.title = time.format('YYYY-MM-DD HH:mm:ss ZZ');
     x.innerText = time.fromNow();
+    x.onclick = function() {
+      this.innerText = this.innerText == this.title ? time.fromNow() : this.title;
+    }
   }
 })(document.querySelectorAll.bind(document));

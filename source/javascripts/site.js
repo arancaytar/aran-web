c8a2dad2a42ef4aa24885dd31b46499a719852b5
Aran <aran@ermarian.net>
2018-03-15 12:29:14 +0100

    Absolute times: Global and persistent.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,27 @@
 ($$ => {
-  for (let x of $$('time')) {
-    const time = moment(x.dateTime);
-    x.title = time.format('YYYY-MM-DD HH:mm:ss ZZ');
-    x.innerText = time.fromNow();
-    x.onclick = function() {
-      this.innerText = this.innerText == this.title ? time.fromNow() : this.title;
+  const storage = window.localStorage;
+
+  const click = function() {
+    const abs = !!+(storage.absoluteTimes = +!+storage.absoluteTimes);
+    update(abs, $$('time'));
+  };
+
+  const update = (abs, times) => {
+    for (let x of times) {
+      const time = moment(x.dateTime);
+      x.innerText = abs ? x.title : time.fromNow();
     }
-  }
+  };
+
+  const init = () => {
+    const times = $$('time');
+    for (let x of times) {
+      const time = moment(x.dateTime);
+      x.title = time.format('YYYY-MM-DD HH:mm:ss ZZ');
+      x.onclick = click;
+    }
+    update(!!+storage.absoluteTimes, times);
+  };
+  init();
+
 })(document.querySelectorAll.bind(document));
